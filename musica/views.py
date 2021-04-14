37b22f50e5d610b4b24e35c98cd4bee42096b460
Lucas Santus <leos9877@gmail.com>
2021-04-14 11:57:12 -0300

    inserindo alterações ao template index

--- musica/views.py
+++ musica/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 import requests
 import json
 
 def index(request):
-    try:
+
+    api_request = requests.get("http://127.0.0.1:8000/api/list/?search=")
+    list_musicas = json.loads(api_request.text)
     
-        if request.POST:
-            pesquisa = request.POST.get("pesquisa", None)
-            api_request = requests.get("http://127.0.0.1:8000/api/list/?search="+pesquisa)
-            list_musicas = json.loads(api_request.text)
-        
-        else:
+    if request.POST:
+        pesquisa = request.POST.get("pesquisa", None)
+                            
+        api_request = None
+        list_musicas = None
+
+        api_request = requests.get("http://127.0.0.1:8000/api/list/?search="+pesquisa)
+        list_musicas = json.loads(api_request.text)
+
+        if not list_musicas: 
             api_request = requests.get("http://127.0.0.1:8000/api/music/?format=json")
             list_musicas = json.loads(api_request.text)
 
-    except:
-        list_musicas = None
+            messages.error(request, "Música não encontrada!")
 
     context = {
         "list_musicas": list_musicas,
